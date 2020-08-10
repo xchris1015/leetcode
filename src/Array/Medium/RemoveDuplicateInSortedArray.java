5cb15ee41a3ce05948a9a4406f1fb16bbfb07694
Chris Xu <xchris1015@gmail.com>
2020-08-10 18:07:49 -0400

    RemoveDuplicatesInSortedArray and with K hold length change version

--- src/Array/Medium/RemoveDuplicateInSortedArray.java
+++ src/Array/Medium/RemoveDuplicateInSortedArray.java
@@ -2,9 +2,9 @@ package Array.Medium;
 
 public class RemoveDuplicateInSortedArray {
 
-    // a b c b b b c c c
-    //                   f
-    //       s
+    // a a a b b b c c c
+    //       f
+    //   s
 
 //    public String removeDuplicates(String string) { //TODO generic
 //        // edge case
@@ -23,7 +23,7 @@ public class RemoveDuplicateInSortedArray {
 //
 //        for (fast = 1 ; fast < chars.length ; fast++ ) {
 //
-//            if (chars[fast] == chars[fast - 1]) {
+//            if (chars[fast] == chars[slow - 1]) {
 //                continue;
 //            }
 //
@@ -34,33 +34,65 @@ public class RemoveDuplicateInSortedArray {
 //
 //    }
 
-    public String removeDuplicatesHoldK(String string, int k) { //TODO generic
-        // edge case
-        if (string == null || string.length() == 0) {
-            return new String();
-        }
+//    public String removeDuplicatesHoldK(String string, int k) { //TODO generic
+//        // edge case
+//        if (string == null || string.length() == 0) {
+//            return new String();
+//        }
+//
+//        // base case
+//        if (string.length() <= k) {
+//            return string;
+//        }
+//
+//        char[] chars = string.toCharArray();
+//        int slow = k;
+//        int fast;
+//
+//        for (fast = k ; fast < chars.length ; fast++ ) {
+//
+//            if (chars[fast] == chars[slow - k]) {
+//                continue;
+//            }
+//
+//            chars[slow++] = chars[fast];
+//        }
+//
+//        return new String(chars, 0, slow);
+//
+//    }
 
-        // base case
-        if (string.length() <= k) {
-            return string;
-        }
+    //[0, s] approach
 
-        char[] chars = string.toCharArray();
-        int slow = k;
-        int fast;
+//    public String removeDuplicatesHoldK(String string, int k) { //TODO generic
+//        // edge case
+//        if (string == null || string.length() == 0) {
+//            return new String();
+//        }
+//
+//        // base case
+//        if (string.length() <= k) {
+//            return string;
+//        }
+//
+//        char[] chars = string.toCharArray();
+//        int slow = k - 1;
+//        int fast;
+//
+//        for (fast = k ; fast < chars.length ; fast++ ) {
+//
+//            if (chars[fast] == chars[slow - k + 1]) {
+//                continue;
+//            }
+//
+//            chars[++slow] = chars[fast];
+//        }
+//
+//        return new String(chars, 0, slow + 1);
+//
+//    }
 
-        for (fast = k ; fast < chars.length ; fast++ ) {
 
-            if (chars[fast] == chars[fast - k]) {
-                continue;
-            }
-
-            chars[slow++] = chars[fast];
-        }
-
-        return new String(chars, 0, slow);
-
-    }
 
 
 
