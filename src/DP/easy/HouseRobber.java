7cd702bf29fcbc7b724bfab488a172b2264d4c8a
Chris Xu <xchris1015@gmail.com>
2020-08-19 00:11:04 -0400

    HouseRobber

---
+++ src/DP/easy/HouseRobber.java
@@ -0,0 +1,24 @@
+package DP.easy;
+
+public class HouseRobber {
+
+    //On
+
+    public int rob(int[] nums) {
+        // function : dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2 ; i < dp.length ; i++ ) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+
+        return dp[dp.length - 1];
+    }
+}
